a5903b733d9e996d3a5e2d9654fcc97d63e2837c
AlexSkellington <131292380+AlexSkellington@users.noreply.github.com>
2024-12-03 16:17:17 -0500

    Update TBS_Maintenace_Script_V2.ps1

--- TBS_Maintenace_Script_V2.ps1
+++ TBS_Maintenace_Script_V2.ps1
@@ -6372,7 +6372,7 @@ if (-not $SilentMode)
 		
 		# Create the main form
 		$form = New-Object System.Windows.Forms.Form
-		$form.Text = "Created by Alex_C.T - Version 1.4"
+		$form.Text = "Created by Alex_C.T - Version 1.5"
 		$form.Size = New-Object System.Drawing.Size(1005, 710)
 		$form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
 		
