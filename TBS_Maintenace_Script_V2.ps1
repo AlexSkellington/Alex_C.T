f78361b285f039451d94f0667e1dc2aeff57bbfb
AlexSkellington <131292380+AlexSkellington@users.noreply.github.com>
2025-02-12 17:36:33 -0500

    Update TBS_Maintenace_Script_V2.ps1

--- TBS_Maintenace_Script_V2.ps1
+++ TBS_Maintenace_Script_V2.ps1
@@ -8713,8 +8713,7 @@ function Retrive_Transactions
 	# --------------------------------------------------
 	$SQIContent = @"
 @WIZSET(DETAIL=D);
-@WIZSET(START=$startDateFormatted);
-@WIZSET(STOP=$stopDateFormatted);
+@WIZDATES(START=$startDateFormatted,STOP=$stopDateFormatted);
 
 @WIZFIL(FIL1=0,FIL2=99999999,SELECT F1032,F254,F1036 FROM SAL_HDR@WIZGET(TRANS_LOCAL) 
 WHERE F1067='CLOSE' and  F254>='@WIZGET(START)' and F254<='@WIZGET(STOP)'
