3f7d4d8a5b8520ec4347839aae88ff38e53845c4
AlexSkellington <131292380+AlexSkellington@users.noreply.github.com>
2025-07-01 13:28:26 -0400

    Update TBS_Maintenace_Script_V2.ps1

--- TBS_Maintenace_Script_V2.ps1
+++ TBS_Maintenace_Script_V2.ps1
@@ -20,7 +20,7 @@ Write-Host "Script starting, pls wait..." -ForegroundColor Yellow
 
 # Script build version (cunsult with Alex_C.T before changing this)
 $VersionNumber = "2.2.9"
-$VersionDate = "2025-07-02"
+$VersionDate = "2025-07-01"
 
 # Retrieve Major, Minor, Build, and Revision version numbers of PowerShell
 $major = $PSVersionTable.PSVersion.Major
